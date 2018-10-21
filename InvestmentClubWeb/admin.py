7dd80ba761c42b40cd5ceeb3157d2a3ec78ab151
J.A. Simmons V <simmonsj@jasimmonsv.com>
2018-10-20 23:20:47 -0500

    init commit

---
+++ InvestmentClubWeb/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Stock)
+admin.site.register(models.StockAsset)
+admin.site.register(models.Ledger)
---
+++ InvestmentClubWeb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InvestmentclubwebConfig(AppConfig):
+    name = 'InvestmentClubWeb'
